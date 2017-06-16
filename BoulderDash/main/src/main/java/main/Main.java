8c75e77aea02e75407a65f0b1b020a55372485ee
Peter <peter.giromagny@viacesi.fr>
2017-06-16 09:40:11 +0200

    Jpanel JFRAME sprite test

--- BoulderDash/main/src/main/java/main/Main.java
+++ BoulderDash/main/src/main/java/main/Main.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import controller.ControllerFacade;
 import model.ModelFacade;
 import view.ViewFacade;
+import view.ViewPanel;
 
 /**
  * <h1>The Class Main.</h1>
@@ -21,6 +22,9 @@ public abstract class Main {
      *            the arguments
      */
     public static void main(final String[] args) {
+    	
+    	ViewPanel view = new ViewPanel();
+    	
         final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
 
         try {
--- BoulderDash/view/src/main/java/view/ViewPanel.java
+++ BoulderDash/view/src/main/java/view/ViewPanel.java
@@ -1,28 +1,28 @@
 package view;
 
+
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+
  
 public class ViewPanel extends JPanel {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	Image SpriteBonhomme;
 	//Image img = SpriteBonhomme;
 	
   public void paintComponent(Graphics g){
     try {
-    	Image img = ImageIO.read(new File("C8.PNG"));
+    	Image img = ImageIO.read(new File("/Users/Peter/Documents/Ecole/eXia/UE - Java/Projet/sprite personnage/C9.PNG"));
       //Pour une image de fond
-      g.drawImage(img, 0, 0, this);
+      g.drawImage(img, 50, 50, this);
     } catch (IOException e) {
       e.printStackTrace();
     }                
-  }               
+  }
+             
 }
