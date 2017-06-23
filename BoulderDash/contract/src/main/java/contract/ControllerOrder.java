e89d922d0f7928900d0d969395d9574cc25efafd
BlackFantasy <arnaud.luczak@viacesi.fr>
2017-06-23 11:25:00 +0200

    New Test

---
+++ BoulderDash/contract/src/main/java/contract/ControllerOrder.java
@@ -0,0 +1,18 @@
+package contract;
+
+public enum ControllerOrder {
+
+	
+	/**
+	 * The Enum ControllerOrder.
+	 *
+	 * @author Jean-Aymeric Diet
+	 */
+
+		/** The English. */
+		English, /** The French. */
+		Francais, /** The Deutch. */
+		Deutsch, /** The Indonesia. */
+		Indonesia,/** cinque */
+		cinque
+	}
